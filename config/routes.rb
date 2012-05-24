cd008a4caa9983b9b353d19818f54cc7e91f1b66
Emily Ellison <emilyellison986@gmail.com>
2012-05-23 23:28:01 -0500

    Add Sign In Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ TeamBeerOn::Application.routes.draw do
   
   root to: 'beer_me#location'
   
+  get 'sign_in' => 'sessions#new', as: :sign_in
+  get 'sign_out' => 'sessions#destroy', as: :sign_out
   get 'beers/1/rate' => 'beers#rate', as: :rate_beer 
-  get 'signup' => 'members#new', action: 'new', as: :sign_up
+  get 'sign_up' => 'members#new', action: 'new', as: :sign_up
   get 'members/1' => 'members#show', as: :show_member
   get 'members/1/edit' => 'members#edit', as: :edit_member
   get 'contact' => 'information#contact'
