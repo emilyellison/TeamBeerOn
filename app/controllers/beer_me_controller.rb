4577c628551af930413e66695982d8c433ae3946
Emily Ellison <emilyellison986@gmail.com>
2012-06-09 14:45:42 -0500

    Clean Up Beer Me Controller

--- app/controllers/beer_me_controller.rb
+++ app/controllers/beer_me_controller.rb
@@ -16,11 +16,24 @@ class BeerMeController < ApplicationController
   def preference
     @available_beer = Beer.joins(:beer_experiences).where('beer_experiences.bar_id LIKE ?', params[:bar_id])
     @q = @available_beer.search(params[:q])
-    @@beers = @q.result(:distinct => true)
+    @selected_beer = @q.result(:distinct => true)
     
-    # Create a characteristics array for form labeling and collecting how many 
-    # beers are left in the database for any particular characteristic.
+    styles = [] 
+    if params[:q]
+      @sum_of_checkboxes_checked = params[:q].values.map{ |x| x.to_i }.sum
+      if @sum_of_checkboxes_checked > 0
+        @selected_beer.all.collect { |x| styles << x.style }
+      else 
+        @available_beer.all.collect { |x| styles << x.style }
+      end
+    else
+      @sum_of_checkboxes_checked = 0
+      @available_beer.all.collect { |x| styles << x.style }
+    end
+    @styles = styles.uniq.sort!
     
+    # Characteristics - Still need cleaning...
+  
     @characteristics_array = [] 
     characteristics = [ :acidic, :clean, :creamy, :crisp, :hoppy, :malty, :rich, :smooth, :bitter, :earthy,
       :sour, :spicy, :sweet, :tart, :banana, :caramel, :citrus, :chocolate, :cloves, :coffee, :floral, :fruity,
@@ -35,30 +48,9 @@ class BeerMeController < ApplicationController
           search_term: "#{char.to_s}_present".to_sym, 
           
           # Create the number of beers behind each characteristic.
-          number_of_beers: @@beers.sum(char) } } 
-    end
- 
-    # Add the number of checkboxes checked.
-    
-    if params[:q]
-      @sum_of_checkboxes_checked = params[:q].values.map{ |x| x.to_i }.sum
-    else
-      @sum_of_checkboxes_checked = 0
-    end
-      
-    # If they have filtered on any checkbox characteristics, only keep the styles left.
-    # Otherwise, load all the styles.  
-      
-    styles = []  
-    if @sum_of_checkboxes_checked > 0
-      @@beers.all.collect { |x| styles << x.style }
-    else 
-      @available_beer.all.collect { |x| styles << x.style }
+          number_of_beers: @selected_beer.sum(char) } } 
     end
     
-    # Uniqify styles and sort alphabetically.
-  
-    @styles = styles.uniq.sort!
     
   end
   
