c83ea36e95fa61b7069b2133d11d0bddf0dbb8db
Emily Ellison <emilyellison986@gmail.com>
2012-06-09 14:16:55 -0500

    Add Characteristics Methods to the Beer Model & Change Characteristics Action to Has_Characteristics

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -8,7 +8,7 @@ class Beer < ActiveRecord::Base
   validates :name, presence: true
   validates :brewery_id, presence: true
   
-  def characteristics
+  def has_characteristics
     available_characteristics = { 'Acidic' => acidic, 'Clean' => clean, 'Creamy' => creamy, 'Crisp' => crisp, 
       'Hoppy' => hoppy, 'Malty' => malty, 'Rich' => rich, 'Smooth' => smooth, 'Bitter' => bitter, 'Earthy' => earthy,
       'Sour' => sour, 'Spicy' => spicy, 'Sweet' => sweet, 'Tart' => tart, 'Banana' => banana, 'Caramel' => caramel, 
@@ -23,5 +23,24 @@ class Beer < ActiveRecord::Base
     end
     has_characteristics
   end
+  
+  def self.characteristics
+    characteristics = [ :acidic, :clean, :creamy, :crisp, :hoppy, :malty, :rich, :smooth, :bitter, :earthy,
+      :sour, :spicy, :sweet, :tart, :banana, :caramel, :citrus, :chocolate, :cloves, :coffee, :floral, :fruity,
+      :grapefruit, :lemon, :nutty, :pine, :smoky, :toffee, :vanilla, :wheat, :belgian ]
+  end
+  
+  def self.label(char)
+    char.to_s.capitalize
+  end
+  
+  def self.search_term(char)
+    "#{char.to_s}_present".to_sym
+  end
+  
+  def self.number_of_beers(proxy, char)
+    proxy.sum(char)
+  end
+  
 end
 
