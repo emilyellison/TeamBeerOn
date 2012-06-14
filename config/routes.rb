dbe86cdec1b137c2faa06c402c1228f989705361
Emily Ellison <emilyellison986@gmail.com>
2012-06-14 09:31:08 -0500

    Set up Static Pages Controller & Routes (About & Home Page)

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ TeamBeerOn::Application.routes.draw do
   
   resources :ratings
 
-  root to: 'beer_me#location'
+  root to: 'static_pages#home'
   
 	resources :sessions
 	resources :members
+	
+	get '' => 'static_pages#home'
+	get 'about' => 'static_pages#about', as: :about
 
   get 'members/:id' => 'members#show', as: :member	
 	get 'sign_up' => 'members#new', as: :sign_up
@@ -15,6 +18,7 @@ TeamBeerOn::Application.routes.draw do
   get "sign_out" => 'sessions#destroy', :as => :sign_out
   post "sessions/create" => 'sessions#create'
 
+  get 'location' => 'beer_me#location', as: :location
   get 'preference' => 'beer_me#preference', as: :beers
   post 'location' => 'beer_me#pass_location', as: :pass_location
   get 'recommendation' => 'beer_me#recommendation', as: :recommendation
