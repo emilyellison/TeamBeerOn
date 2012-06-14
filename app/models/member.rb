baad8da535716a62ef6804fbaf414488fdad7ca3
Emily Ellison <emilyellison986@gmail.com>
2012-06-14 09:28:32 -0500

    Add Member Validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,5 +4,9 @@ class Member < ActiveRecord::Base
   has_secure_password
 
   has_many :ratings
+  
+  validates :name, presence: true, length: { maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
 
 end
