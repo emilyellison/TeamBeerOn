6ade23c265db7aeb8b3a59a9a56ea5dda445f6a2
Emily Ellison <emilyellison986@gmail.com>
2012-06-16 10:55:04 -0500

    Show Twitter Feed with #beeron2012 on Users Profile Page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class MembersController < ApplicationController
  
   def new
@@ -6,6 +8,15 @@ class MembersController < ApplicationController
 
   def show
   	@member = Member.find(params[:id])
+  	@json = JSON.parse(open('http://search.twitter.com/search.json?q=beeron2012&rpp=5&include_entities=true&with_twitter_user_id=true&result_type=mixed').read)
+    @tweets = []
+    @json['results'].each do |json| 
+      @tweets << { created_at: json['created_at'], 
+                   text: json['text'], 
+                   hashtags: json['entities']['hashtags'],
+                   from_user: json['from_user'],
+                   from_user_name: json['from_user_name'] }
+    end
   end
   
   def edit
