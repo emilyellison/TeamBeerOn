5d55b226a7ab3de6c39d54a1f41bc7f3bcc080f1
Emily Ellison <emilyellison986@gmail.com>
2012-06-17 13:56:17 -0500

    Add Edit Link to Show Page for Ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,7 +12,7 @@ class RatingsController < ApplicationController
 
   def edit
     @rating = Rating.find(params[:id])
-    @beer = Beer.find_by_id(params[:beer_id])
+    @beer = Beer.find(@rating.beer.id)
   end
 
   def create
