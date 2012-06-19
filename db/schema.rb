6d0e7cec229dcee634fb162c73b2f36938130c21
Emily Ellison <emilyellison986@gmail.com>
2012-06-19 08:39:45 -0500

    Add Stout Brewery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120616214716) do
+ActiveRecord::Schema.define(:version => 20120618022308) do
 
   create_table "bars", :force => true do |t|
     t.string   "name"
@@ -161,6 +161,8 @@ ActiveRecord::Schema.define(:version => 20120616214716) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "members", ["email"], :name => "index_members_on_email", :unique => true
 
   create_table "ratings", :force => true do |t|
     t.integer  "score"
